b958f13fb15745863906e0e553a2c73877cbe152
terasi <hitokunn750@gmail.com>
2024-09-26 20:32:59 +0900

    zissou ensyu3

---
+++ Iphone.java
@@ -0,0 +1,14 @@
+
+public class Iphone {
+
+	public static void main(String[] args) {
+
+		SmartPhone iPhone=new SmartPhone();
+		iPhone.play();
+		iPhone.stop();
+		iPhone.next();
+		iPhone.back();
+
+	}
+
+}
